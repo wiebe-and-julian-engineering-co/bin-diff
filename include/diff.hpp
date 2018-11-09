0d77385f9e51d3faa4e811385c7a7ce55279d383
Julian van Doorn <j.g.doornvan@gmail.com>
2018-11-09 17:45:07 +0100

    Removed unnecessary diff class and added some doxygen

--- include/diff.hpp
+++ include/diff.hpp
@@ -239,152 +239,174 @@ class Patch {
     }
 };
 
-class Diff {
-    template <class t_sequenced_type>
-    void _diff(const sequence_view<t_sequenced_type>& lhs_seq, const sequence_view<t_sequenced_type>& rhs_seq, std::vector<Patch>& patches) const {
-        int32_t lhs_size = lhs_seq.size();
-        int32_t rhs_size = rhs_seq.size();
-        int32_t max_len = lhs_size + rhs_size;
-        int32_t x_values_len = 2 * std::min(lhs_size, rhs_size) + 2;
+namespace diff {
+    namespace {
+        /**
+         * @brief Optimal diff algorithm
+         * 
+         * @details
+         * Based off of Robert Elder's optimized Myer's diff
+         * See more: http://blog.robertelder.org/diff-algorithm/
+         * 
+         * @tparam t_sequenced_type Type contained in the sequence
+         * @param lhs_seq Source sequence
+         * @param rhs_seq Destination sequence to generate patches to
+         * @param patches Patches holding vector
+         */
+        template <class t_sequenced_type>
+        void _diff(const sequence_view<t_sequenced_type>& lhs_seq, const sequence_view<t_sequenced_type>& rhs_seq, std::vector<Patch>& patches) {
+            int32_t lhs_size = lhs_seq.size();
+            int32_t rhs_size = rhs_seq.size();
+            int32_t max_len = lhs_size + rhs_size;
+            int32_t x_values_len = 2 * std::min(lhs_size, rhs_size) + 2;
 
-        int32_t x = 0;
-        int32_t y = 0;
-        int32_t z = 0;
-        int32_t x_initial = 0;
-        int32_t y_initial = 0;
-        int32_t u = 0;
-        int32_t v = 0;
+            int32_t x = 0;
+            int32_t y = 0;
+            int32_t z = 0;
+            int32_t x_initial = 0;
+            int32_t y_initial = 0;
+            int32_t u = 0;
+            int32_t v = 0;
 
-        if (lhs_size > 0 && rhs_size > 0) {
-            int32_t w = lhs_size - rhs_size;
+            if (lhs_size > 0 && rhs_size > 0) {
+                int32_t w = lhs_size - rhs_size;
 
-            std::vector<int32_t> best_forward_x_values(x_values_len, 0);
-            std::vector<int32_t> best_backward_x_values(x_values_len, 0);
+                std::vector<int32_t> best_forward_x_values(x_values_len, 0);
+                std::vector<int32_t> best_backward_x_values(x_values_len, 0);
 
-            for (int32_t D = 0; D < (max_len / 2 + (max_len % 2 != 0)) + 1; D++) {
-                // forward snake
-                for (int32_t k = -(D - 2 * std::max(0, D - rhs_size)); k < D - 2 * std::max(0, D - lhs_size) + 1; k += 2) {
-                    if (k == -D || (k != D && best_forward_x_values[(k - 1) % x_values_len] < best_forward_x_values[(k + 1) % x_values_len])) {
-                        x = best_forward_x_values[(k + 1) % x_values_len];
-                    } else {
-                        x = best_forward_x_values[(k - 1) % x_values_len] + 1;
-                    }
-
-                    y = x - k;
-                    x_initial = x;
-                    y_initial = y;
-
-                    while (x < lhs_size && y < rhs_size && lhs_seq[x] == rhs_seq[y]) { // evaluate diagonals
-                        x++;
-                        y++;
-                    }
-
-                    best_forward_x_values[k % x_values_len] = x;
-                    z = -(k - w);
-
-                    if (max_len % 2 == 1 && z >= -(D - 1) && z <= D - 1 && best_forward_x_values[k % x_values_len] + best_backward_x_values[z % x_values_len] >= lhs_size) {
-                        D = 2 * D - 1;
-                        u = x;
-                        v = y;
-                        x = x_initial;
-                        y = y_initial;
-
-                        if (D > 1 || (x != u && y != v)) {
-                            // forward subdiff
-                            _diff(
-                                sequence_view(lhs_seq, 0, x),
-                                sequence_view(rhs_seq, 0, y),
-                                patches
-                            );
-
-                            // backward subdiff
-                            _diff(
-                                sequence_view(lhs_seq, u, lhs_size),
-                                sequence_view(rhs_seq, v, rhs_size),
-                                patches
-                            );
-
-                            return;
-                        }
-                        else if (rhs_size > lhs_size) {
-                            _diff(sequence_view<t_sequenced_type>(), sequence_view(rhs_seq, lhs_size, rhs_size), patches);
-                            return;
-                        } else if (rhs_size < lhs_size) {
-                            _diff(sequence_view(lhs_seq, rhs_size, lhs_size), sequence_view<t_sequenced_type>(), patches);
-                            return;
+                for (int32_t D = 0; D < (max_len / 2 + (max_len % 2 != 0)) + 1; D++) {
+                    // forward snake
+                    for (int32_t k = -(D - 2 * std::max(0, D - rhs_size)); k < D - 2 * std::max(0, D - lhs_size) + 1; k += 2) {
+                        if (k == -D || (k != D && best_forward_x_values[(k - 1) % x_values_len] < best_forward_x_values[(k + 1) % x_values_len])) {
+                            x = best_forward_x_values[(k + 1) % x_values_len];
                         } else {
-                            return;
+                            x = best_forward_x_values[(k - 1) % x_values_len] + 1;
+                        }
+
+                        y = x - k;
+                        x_initial = x;
+                        y_initial = y;
+
+                        while (x < lhs_size && y < rhs_size && lhs_seq[x] == rhs_seq[y]) { // evaluate diagonals
+                            x++;
+                            y++;
+                        }
+
+                        best_forward_x_values[k % x_values_len] = x;
+                        z = -(k - w);
+
+                        if (max_len % 2 == 1 && z >= -(D - 1) && z <= D - 1 && best_forward_x_values[k % x_values_len] + best_backward_x_values[z % x_values_len] >= lhs_size) {
+                            D = 2 * D - 1;
+                            u = x;
+                            v = y;
+                            x = x_initial;
+                            y = y_initial;
+
+                            if (D > 1 || (x != u && y != v)) {
+                                // forward subdiff
+                                _diff(
+                                    sequence_view(lhs_seq, 0, x),
+                                    sequence_view(rhs_seq, 0, y),
+                                    patches
+                                );
+
+                                // backward subdiff
+                                _diff(
+                                    sequence_view(lhs_seq, u, lhs_size),
+                                    sequence_view(rhs_seq, v, rhs_size),
+                                    patches
+                                );
+
+                                return;
+                            }
+                            else if (rhs_size > lhs_size) {
+                                _diff(sequence_view<t_sequenced_type>(), sequence_view(rhs_seq, lhs_size, rhs_size), patches);
+                                return;
+                            } else if (rhs_size < lhs_size) {
+                                _diff(sequence_view(lhs_seq, rhs_size, lhs_size), sequence_view<t_sequenced_type>(), patches);
+                                return;
+                            } else {
+                                return;
+                            }
+                        }
+                    }
+
+                    // backward snake
+                    for (int32_t k = -(D - 2 * std::max(0, D - rhs_size)); k < D - 2 * std::max(0, D - lhs_size) + 1; k += 2) {
+                        if (k == -D || (k != D && best_backward_x_values[(k - 1) % x_values_len] < best_backward_x_values[(k + 1) % x_values_len])) {
+                            x = best_backward_x_values[(k + 1) % x_values_len];
+                        } else {
+                            x = best_backward_x_values[(k - 1) % x_values_len] + 1;
+                        }
+
+                        y = x - k;
+                        x_initial = x;
+                        y_initial = y;
+
+                        while (x < lhs_size && y < rhs_size && lhs_seq[lhs_size - x - 1] == rhs_seq[rhs_size - y - 1]) { // evaluate diagonals
+                            x++;
+                            y++;
+                        }
+
+                        best_backward_x_values[k % x_values_len] = x;
+                        z = -(k - w);
+
+                        if (max_len % 2 == 0 && z >= -D && z <= D && best_backward_x_values[k % x_values_len] + best_forward_x_values[z % x_values_len] >= lhs_size) {
+                            D = 2 * D;
+                            u = lhs_size - x_initial;
+                            v = rhs_size - y_initial;
+                            x = lhs_size - x;
+                            y = rhs_size - y;
+
+                            if (D > 1 || (x != u && y != v)) {
+                                // forward subdiff
+                                _diff(
+                                    sequence_view(lhs_seq, 0, x),
+                                    sequence_view(rhs_seq, 0, y),
+                                    patches
+                                );
+
+                                // backward subdiff
+                                _diff(
+                                    sequence_view(lhs_seq, u, lhs_size),
+                                    sequence_view(rhs_seq, v, rhs_size),
+                                    patches
+                                );
+
+                                return;
+                            } else if (rhs_size > lhs_size) {
+                                _diff(sequence_view<t_sequenced_type>(), sequence_view(rhs_seq, lhs_size, rhs_size), patches);
+                                return;
+                            } else if (rhs_size < lhs_size) {
+                                _diff(sequence_view(lhs_seq, rhs_size, lhs_size), sequence_view<t_sequenced_type>(), patches);
+                                return;
+                            } else {
+                                return;
+                            }
                         }
                     }
                 }
-
-                // backward snake
-                for (int32_t k = -(D - 2 * std::max(0, D - rhs_size)); k < D - 2 * std::max(0, D - lhs_size) + 1; k += 2) {
-                    if (k == -D || (k != D && best_backward_x_values[(k - 1) % x_values_len] < best_backward_x_values[(k + 1) % x_values_len])) {
-                        x = best_backward_x_values[(k + 1) % x_values_len];
-                    } else {
-                        x = best_backward_x_values[(k - 1) % x_values_len] + 1;
-                    }
-
-                    y = x - k;
-                    x_initial = x;
-                    y_initial = y;
-
-                    while (x < lhs_size && y < rhs_size && lhs_seq[lhs_size - x - 1] == rhs_seq[rhs_size - y - 1]) { // evaluate diagonals
-                        x++;
-                        y++;
-                    }
-
-                    best_backward_x_values[k % x_values_len] = x;
-                    z = -(k - w);
-
-                    if (max_len % 2 == 0 && z >= -D && z <= D && best_backward_x_values[k % x_values_len] + best_forward_x_values[z % x_values_len] >= lhs_size) {
-                        D = 2 * D;
-                        u = lhs_size - x_initial;
-                        v = rhs_size - y_initial;
-                        x = lhs_size - x;
-                        y = rhs_size - y;
-
-                        if (D > 1 || (x != u && y != v)) {
-                            // forward subdiff
-                            _diff(
-                                sequence_view(lhs_seq, 0, x),
-                                sequence_view(rhs_seq, 0, y),
-                                patches
-                            );
-
-                            // backward subdiff
-                            _diff(
-                                sequence_view(lhs_seq, u, lhs_size),
-                                sequence_view(rhs_seq, v, rhs_size),
-                                patches
-                            );
-
-                            return;
-                        } else if (rhs_size > lhs_size) {
-                            _diff(sequence_view<t_sequenced_type>(), sequence_view(rhs_seq, lhs_size, rhs_size), patches);
-                            return;
-                        } else if (rhs_size < lhs_size) {
-                            _diff(sequence_view(lhs_seq, rhs_size, lhs_size), sequence_view<t_sequenced_type>(), patches);
-                            return;
-                        } else {
-                            return;
-                        }
-                    }
-                }
+            }  else if (lhs_size > 0) {
+                patches.push_back(Patch::make_deletion(lhs_seq));
+                return;
+            } else if (rhs_size > 0) {
+                patches.push_back(Patch::make_addition(rhs_seq));
+                return;
             }
-        }  else if (lhs_size > 0) {
-            patches.push_back(Patch::make_deletion(lhs_seq));
-            return;
-        } else if (rhs_size > 0) {
-            patches.push_back(Patch::make_addition(rhs_seq));
-            return;
         }
     }
 
-  public:
-    Diff() { }
-
+    /**
+     * @brief Optimal diff algorithm
+     * 
+     * @details
+     * Based off of Robert Elder's optimized Myer's diff
+     * See more: http://blog.robertelder.org/diff-algorithm/
+     * 
+     * @param lhs_seq Source sequence
+     * @param rhs_seq Destination sequence to generate patches to
+     * @return std::vector<patch> Vector of calculated patches
+     */
     const auto diff(const std::string& lhs, const std::string& rhs) {
         std::vector<Patch> patches;
         _diff(
@@ -395,4 +417,4 @@ class Diff {
         patches.shrink_to_fit();
         return patches;
     }
-};
+}
--- src/main.cpp
+++ src/main.cpp
@@ -4,19 +4,22 @@
 #include "diff.hpp"
 
 int main() {
-    Diff a;
-
     auto lhs = std::string("goore wormen");
     auto rhs = std::string("guur feesten");
 
     std::cout << lhs << std::endl;
     std::cout << rhs << std::endl;
 
-    auto v = a.diff(lhs, rhs);
+    auto v = diff::diff(lhs, rhs);
 
     for (auto p : v) {
         std::cout << p << std::endl;
     }
 
-    system("pause");
+    // system("pause");
+
+    while (true) { }
+
+    int i = 10;
+    (void) i;
 }
